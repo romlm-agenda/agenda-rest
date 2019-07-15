64a5b5561c1e1da91be69b2d667800187f1d3443
romlm <romain103paris@gmail.com>
2019-07-15 17:23:53 +0200

    Set up basic agenda-eureka server

--- agenda-eureka/src/main/java/org/agenda/eureka/AgendaEurekaApplication.java
+++ agenda-eureka/src/main/java/org/agenda/eureka/AgendaEurekaApplication.java
@@ -2,8 +2,10 @@ package org.agenda.eureka;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 @SpringBootApplication
+@EnableEurekaServer
 public class AgendaEurekaApplication {
 
 	public static void main(String[] args) {

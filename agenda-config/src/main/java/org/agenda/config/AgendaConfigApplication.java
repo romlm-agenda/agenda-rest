16f7ed79ca49fdeab18415928db7706a922e5dd5
romlm <romain103paris@gmail.com>
2019-07-15 16:47:49 +0200

    Add agenda-config empty project

---
+++ agenda-config/src/main/java/org/agenda/config/AgendaConfigApplication.java
@@ -0,0 +1,13 @@
+package org.agenda.config;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AgendaConfigApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AgendaConfigApplication.class, args);
+	}
+
+}

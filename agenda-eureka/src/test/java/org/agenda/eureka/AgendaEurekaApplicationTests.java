832adcedd9ac1b11e99580d4eeebf01b9d31d99a
romlm <romain103paris@gmail.com>
2019-07-15 17:05:27 +0200

    Initial commit for agenda-eureka

---
+++ agenda-eureka/src/test/java/org/agenda/eureka/AgendaEurekaApplicationTests.java
@@ -0,0 +1,16 @@
+package org.agenda.eureka;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class AgendaEurekaApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}

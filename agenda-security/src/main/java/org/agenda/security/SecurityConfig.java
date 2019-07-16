42cca523be0f9da79ac0891d344039a67d273b2e
romlm <romain103paris@gmail.com>
2019-07-16 13:01:47 +0200

    Add provisory security config to security server

---
+++ agenda-security/src/main/java/org/agenda/security/SecurityConfig.java
@@ -0,0 +1,26 @@
+/**
+ * @since 16 juil. 2019
+ */
+package org.agenda.security;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
+
+/**
+ * @author LE MIERE Romain
+ *
+ */
+@EnableWebSecurity
+@Configuration
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+	@Override
+	protected void configure(HttpSecurity http) throws Exception {
+		http.httpBasic().and().csrf().disable().authorizeRequests().anyRequest().fullyAuthenticated().and()
+				.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
+	}
+
+}

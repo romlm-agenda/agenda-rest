8e65724945a27043d038a59e9f6b3ddc65feccad
romlm <romain103paris@gmail.com>
2019-07-16 17:48:27 +0200

    add the token controller

---
+++ agenda-security/src/main/java/org/agenda/security/controllers/TokenController.java
@@ -0,0 +1,45 @@
+/**
+ * @since 16 juil. 2019
+ */
+package org.agenda.security.controllers;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.agenda.security.utils.UniqueIdGenerator;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author LE MIERE Romain
+ *
+ */
+@RestController
+@RequestMapping("/token/")
+public class TokenController {
+
+	private UniqueIdGenerator generator = new UniqueIdGenerator();
+	private List<String> tokens = new ArrayList<>();
+
+	@GetMapping
+	public String getToken() {
+		String token = generator.generateRandomId();
+		tokens.add(token);
+		return token;
+	}
+
+	@PostMapping
+	public boolean isTokenValid(@RequestBody String token) {
+		return tokens.contains(token);
+	}
+	
+	@DeleteMapping
+	public void deleteToken(@RequestBody String token) {
+		tokens.remove(token);
+	}
+
+}

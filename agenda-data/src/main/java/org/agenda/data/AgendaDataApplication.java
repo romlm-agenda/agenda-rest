3cc2eb105a19c2ef22a67789fada20685aee45f6
romlm <romain103paris@gmail.com>
2019-07-17 16:26:00 +0200

    agenda-data - Initial commit for agenda-data

---
+++ agenda-data/src/main/java/org/agenda/data/AgendaDataApplication.java
@@ -0,0 +1,13 @@
+package org.agenda.data;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AgendaDataApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AgendaDataApplication.class, args);
+	}
+
+}

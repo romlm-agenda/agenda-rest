cb985bc0f71de4d35b07098d6b03dfdedac8a016
romlm <romain103paris@gmail.com>
2019-07-18 16:40:34 +0200

    agenda-data - fix missing import in userController

--- agenda-data/src/main/java/org/agenda/data/controllers/UserController.java
+++ agenda-data/src/main/java/org/agenda/data/controllers/UserController.java
@@ -4,7 +4,9 @@
 package org.agenda.data.controllers;
 
 import org.agenda.data.services.UserService;
+import org.agenda.model.User;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -23,7 +25,7 @@ public class UserController {
 
 	@GetMapping
 	public ResponseEntity<User> loginUser(@RequestParam String email, @RequestParam String password) {
-		return 
+		return ResponseEntity.ok(new User());
 
 	}
 

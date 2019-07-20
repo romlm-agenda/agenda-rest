549d128b20998ebe01b9893c56d1a45887067646
romlm <romain103paris@gmail.com>
2019-07-20 12:36:15 +0200

    agenda-gateway - fix null exception for SecurityProxy in TokenGateway

--- agenda-data/src/main/java/org/agenda/data/config/filters/TokenGateway.java
+++ agenda-data/src/main/java/org/agenda/data/config/filters/TokenGateway.java
@@ -6,16 +6,19 @@ package org.agenda.data.config.filters;
 import java.io.IOException;
 import java.util.Optional;
 
-import javax.inject.Inject;
 import javax.servlet.FilterChain;
+import javax.servlet.ServletContext;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpFilter;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 import org.agenda.data.proxies.SecurityProxy;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.client.HttpClientErrorException;
+import org.springframework.web.context.WebApplicationContext;
+import org.springframework.web.context.support.WebApplicationContextUtils;
 
 /**
  * @author LE MIERE Romain
@@ -28,12 +31,19 @@ public class TokenGateway extends HttpFilter {
 	 */
 	private static final long serialVersionUID = -8611232415010862729L;
 
-	@Inject
+	@Autowired
 	private SecurityProxy security;
 
 	@Override
 	protected void doFilter(HttpServletRequest request, HttpServletResponse response, FilterChain chain)
 			throws IOException, ServletException, HttpClientErrorException {
+		if (security == null) {
+			ServletContext servletContext = request.getServletContext();
+			WebApplicationContext webApplicationContext = WebApplicationContextUtils
+					.getWebApplicationContext(servletContext);
+			security = webApplicationContext.getBean(SecurityProxy.class);
+		}
+
 		Optional<String> header = Optional.ofNullable(request.getHeader("zuul-security-header"));
 		if (header.isEmpty() || !security.isTokenValid(header.get()))
 			throw new HttpClientErrorException(HttpStatus.UNAUTHORIZED, "Missing or invalid header");

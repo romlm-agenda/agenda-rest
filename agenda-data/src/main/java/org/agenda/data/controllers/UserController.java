6a7cc58b8cc89d292481c745c1776fb7f2d5d2fa
romlm <romain103paris@gmail.com>
2019-07-21 11:44:10 +0200

    agenda-data - change login method to post

--- agenda-data/src/main/java/org/agenda/data/controllers/UserController.java
+++ agenda-data/src/main/java/org/agenda/data/controllers/UserController.java
@@ -8,7 +8,7 @@ import org.agenda.model.User;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.authentication.BadCredentialsException;
-import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -24,13 +24,14 @@ public class UserController {
 	@Autowired
 	private UserService users;
 
-	@GetMapping("/public/login")
+	@PostMapping("/public/login")
 	public ResponseEntity<User> loginUser(@RequestParam String email, @RequestParam String password) {
 		User user = null;
 		try {
 			user = users.loginUser(email, password);
 		} catch (BadCredentialsException e) {
-			return ResponseEntity.status(404).build();
+			// TODO : Log here
+			return ResponseEntity.notFound().build();
 		}
 		return ResponseEntity.ok(user);
 

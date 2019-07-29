122159a4df12bf7927e373d2864a4bea260cffb2
romlm <romain103paris@gmail.com>
2019-07-29 09:44:07 +0200

    update endpoints for userController now restricted to LocalDates

--- agenda-data/src/main/java/org/agenda/data/controllers/UserController.java
+++ agenda-data/src/main/java/org/agenda/data/controllers/UserController.java
@@ -15,6 +15,7 @@ import org.agenda.model.Day;
 import org.agenda.model.Month;
 import org.agenda.model.User;
 import org.agenda.model.Week;
+import org.agenda.model.Year;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -26,6 +27,7 @@ import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -83,19 +85,26 @@ public class UserController {
 	// private endpoints
 
 	@DeleteMapping("/private/delete")
-	public ResponseEntity<Void> delete(@RequestBody User user)
+	public ResponseEntity<Void> delete(
+	    @RequestHeader("userId") String userId,
+	    @RequestBody User user
+	)
 	{
 		return null;
 	}
 
 	@GetMapping("/private/day")
-	public ResponseEntity<Day> getDayByDate(@RequestParam @DateTimeFormat(iso = DateTimeFormat.ISO.DATE) LocalDate date)
+	public ResponseEntity<Day> getDayByDate(
+	    @RequestHeader("userId") String userId,
+	    @RequestParam @DateTimeFormat(iso = DateTimeFormat.ISO.DATE) LocalDate date
+	)
 	{
 		return null;
 	}
 
 	@GetMapping("/private/days")
 	public ResponseEntity<List<Day>> getDaysBetweenDates(
+	    @RequestHeader("userId") String userId,
 	    @RequestParam @DateTimeFormat(iso = DateTimeFormat.ISO.DATE) LocalDate from,
 	    @RequestParam @DateTimeFormat(iso = DateTimeFormat.ISO.DATE) LocalDate to
 	)
@@ -104,35 +113,18 @@ public class UserController {
 	}
 
 	@GetMapping("/private/week")
-	public ResponseEntity<Week> getWeek(
-	    @RequestParam Integer yearId,
-	    @RequestParam Integer weekId
-	)
-	{
-		return null;
-	}
-
-	@GetMapping("/private/weekByDate")
 	public ResponseEntity<Week> getWeekByDate(
+	    @RequestHeader("userId") String userId,
 	    @RequestParam @DateTimeFormat(iso = DateTimeFormat.ISO.DATE) LocalDate date
 	)
 	{
 		return null;
 	}
 
-	@GetMapping("/private/weeks")
-	public ResponseEntity<List<Week>> getWeeks(
-	    @RequestParam Integer fromYearId,
-	    @RequestParam Integer fromWeekId,
-	    @RequestParam Integer toYearId,
-	    @RequestParam Integer toWeekId
-	)
-	{
-		return null;
-	}
 
-	@GetMapping("/private/weeksByDate")
+	@GetMapping("/private/weeks")
 	public ResponseEntity<List<Week>> getWeeksByDate(
+	    @RequestHeader("userId") String userId,
 	    @RequestParam @DateTimeFormat(iso = DateTimeFormat.ISO.DATE) LocalDate from,
 	    @RequestParam @DateTimeFormat(iso = DateTimeFormat.ISO.DATE) LocalDate to
 	)
@@ -141,17 +133,40 @@ public class UserController {
 	}
 
 	@GetMapping("/private/month")
-	public ResponseEntity<Month> getMonth(
-	    @RequestParam Integer yearId,
-	    @RequestParam Integer monthId
+	public ResponseEntity<Month> getMonthByDate(
+	    @RequestHeader("userId") String userId,
+	    @RequestParam @DateTimeFormat(iso = DateTimeFormat.ISO.DATE) LocalDate date
 	)
 	{
 		return null;
 	}
 
-	@GetMapping("/private/monthByDate")
-	public ResponseEntity<Month> getMonthByDate(
+	
+
+	@GetMapping("/private/months")
+	public ResponseEntity<List<Month>> getMonthsByDate(
+	    @RequestHeader("userId") String userId,
+	    @RequestParam @DateTimeFormat(iso = DateTimeFormat.ISO.DATE) LocalDate from,
+	    @RequestParam @DateTimeFormat(iso = DateTimeFormat.ISO.DATE) LocalDate to
+	)
+	{
+		return null;
+	}
+
+	@GetMapping("/private/year")
+	public Year getYearByDate(
+	    @RequestHeader("userId") String userId,
 	    @RequestParam @DateTimeFormat(iso = DateTimeFormat.ISO.DATE) LocalDate date
+	)
+	{
+		return null;
+	}
+
+	@GetMapping("/private/years")
+	public ResponseEntity<List<Year>> getYearsByDate(
+	    @RequestHeader("userId") String userId,
+	    @RequestParam @DateTimeFormat(iso = DateTimeFormat.ISO.DATE) LocalDate from,
+	    @RequestParam @DateTimeFormat(iso = DateTimeFormat.ISO.DATE) LocalDate to
 	)
 	{
 		return null;

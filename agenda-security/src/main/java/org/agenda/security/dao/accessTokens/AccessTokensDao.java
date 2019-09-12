e7d9a384873e40aa1b26cf210df48a0060d9497d
romlm <romain103paris@gmail.com>
2019-09-12 17:44:22 +0200

    create AccessTokensDao

---
+++ agenda-security/src/main/java/org/agenda/security/dao/accessTokens/AccessTokensDao.java
@@ -0,0 +1,17 @@
+/**
+ * @since 12 sept. 2019
+ */
+package org.agenda.security.dao.accessTokens;
+
+import org.agenda.security.beans.AccessTokenBean;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+/**
+ * @author LE MIERE Romain
+ *
+ */
+public interface AccessTokensDao extends JpaRepository<AccessTokenBean, Long> {
+	
+	boolean existsAccessTokenBeanByAccessToken(String accessToken);
+
+}

66d6821c110ed312ecf8d524c4f747b32e281279
romlm <romain103paris@gmail.com>
2019-09-12 17:36:23 +0200

    create entity  AccessTokenBean

---
+++ agenda-security/src/main/java/org/agenda/security/beans/AccessTokenBean.java
@@ -0,0 +1,21 @@
+/**
+ * @since 12 sept. 2019
+ */
+package org.agenda.security.beans;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+
+/**
+ * @author LE MIERE Romain
+ *
+ */
+@Entity(name = "access_tokens_beans")
+public class AccessTokenBean {
+	
+	@Id
+	@Column(updatable = false, unique = true, nullable = false)
+	private String accessToken;
+
+}

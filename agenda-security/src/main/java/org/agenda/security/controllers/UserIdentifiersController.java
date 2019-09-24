de493ee0dee92a380cf34487a6bf309a7d209666
romlm <romain103paris@gmail.com>
2019-09-24 18:31:04 +0200

    implement logging in UserIds controller

--- agenda-security/src/main/java/org/agenda/security/controllers/UserIdentifiersController.java
+++ agenda-security/src/main/java/org/agenda/security/controllers/UserIdentifiersController.java
@@ -4,6 +4,8 @@
 package org.agenda.security.controllers;
 
 import org.agenda.security.services.userIds.UserIdsService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -20,12 +22,16 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/user/")
 public class UserIdentifiersController {
 
+	private static Logger logger = LoggerFactory.getLogger(UserIdentifiersController.class);
+
 	@Autowired
 	private UserIdsService userIds;
 
 	@GetMapping
 	public String getInstance(@RequestParam("userId") String userId)
 	{
+		logger.info("user token requested for userId: "
+		        + userId);
 		return userIds.getTokenForUser(userId);
 	}
 
@@ -35,13 +41,24 @@ public class UserIdentifiersController {
 	    @RequestParam("userAuthKey") String authKey
 	)
 	{
-		return userIds.isUserValid(userId, authKey);
+		boolean valid = userIds.isUserValid(userId, authKey);
+		if (valid)
+			logger.info("valid user requested for userId: "
+			        + userId);
+		else
+			logger.warn("wrong userId or token given for userId: "
+			        + userId
+			        + " and token: "
+			        + authKey);
+		return valid;
 	}
 
 	@DeleteMapping
 	public void deleteUser(@RequestParam("userId") String userId)
 	{
 		userIds.deleteUser(userId);
+		logger.info("deleted user auth for userId: "
+		        + userId);
 	}
 
 }

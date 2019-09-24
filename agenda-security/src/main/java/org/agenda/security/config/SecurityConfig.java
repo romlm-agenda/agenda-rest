539a8e48306ffc6ff01683825a20c7f716d9f0ca
romlm <romain103paris@gmail.com>
2019-09-24 18:21:36 +0200

    agenda-security - remove the access tokens security

--- agenda-security/src/main/java/org/agenda/security/config/SecurityConfig.java
+++ agenda-security/src/main/java/org/agenda/security/config/SecurityConfig.java
@@ -7,7 +7,6 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.config.http.SessionCreationPolicy;
 
 /**
  * @author LE MIERE Romain
@@ -18,9 +17,11 @@ import org.springframework.security.config.http.SessionCreationPolicy;
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
 	@Override
-	protected void configure(HttpSecurity http) throws Exception {
-		http.httpBasic().and().csrf().disable().authorizeRequests().anyRequest().fullyAuthenticated().and()
-				.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
+	protected void configure(HttpSecurity http) throws Exception
+	{
+		http.httpBasic();
+		http.csrf().disable();
+		http.authorizeRequests().anyRequest().anonymous();
 	}
 
 }

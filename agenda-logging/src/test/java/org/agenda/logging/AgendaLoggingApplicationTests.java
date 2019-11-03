b4d94ccf494bf1213094786f9373c287ad1250dd
romlm <romain103paris@gmail.com>
2019-11-03 14:56:16 +0100

    agenda-logging - set up logging server

---
+++ agenda-logging/src/test/java/org/agenda/logging/AgendaLoggingApplicationTests.java
@@ -0,0 +1,13 @@
+package org.agenda.logging;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AgendaLoggingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

46a24f40a7ce33d61a6fbd66e929f90b35f7c7ff
romlm <romain103paris@gmail.com>
2019-11-03 15:45:08 +0100

    agenda-logging - create logging controller

---
+++ agenda-logging/src/main/java/org/agenda/logging/controllers/LoggingController.java
@@ -0,0 +1,17 @@
+/**
+ * @since 3 nov. 2019
+ */
+package org.agenda.logging.controllers;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author LE MIERE Romain
+ *
+ */
+@RestController
+@RequestMapping("/logging/")
+public class LoggingController {
+
+}
